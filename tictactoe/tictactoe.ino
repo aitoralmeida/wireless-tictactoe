905f945ef6c16d84f61ce7cf237bac0657cbdda9
Aitor Almeida <aitoralmeida@gmail.com>
2014-07-03 12:10:45 +0200

    added player turn logic

--- tictactoe/tictactoe.ino
+++ tictactoe/tictactoe.ino
@@ -35,6 +35,7 @@ Square * squares[9];
 // square status
 int redStatus [] = {0,0,0,0,0,0,0,0,0};
 int greenStatus [] = {0,0,0,0,0,0,0,0,0};
+int previousGreenStatus [] = {0,0,0,0,0,0,0,0,0};
 
 // conditions for victory
 int winConditions [][9] = {{1,1,1,0,0,0,0,0,0},
@@ -67,18 +68,68 @@ void setup() {
   // Show the welcome message
   doWelcome();  
   // Find other player
-  rival_id = findPlayer(player_id);
+  rival_id = findPlayer();
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
   delay(200);
-
   
-  /*
+  // ******** Player turn *******
+  for (int i = 0; i < 9; i++){
+    previousGreenStatus[i] = greenStatus[i];
+  }
+  // Update color status
+  // Check player move, wait until it happens
+  printMsg("Your turn.");
+  boolean waitForMove = true;  
+  int player_move = 0;
+  
+  while (waitForMove){
+    updateBoardStatus();  
+    player_move = checkPlayerMove();
+    if (player_move == 0){
+      // No move, wait and check again
+      delay(30);
+    }else if (player_move > 0){
+      // Legal move, continue
+      waitForMove = false;
+    } else if (player_move == -1){
+      // Player has moved 2 pieces, illegal move      
+      msgAndWait("You moved two pieces! Correct you move.");      
+    } else if (player_move == -2){
+      // Player has moved the piece into an square already used.     
+      msgAndWait("You moved your piece into an already used square! Correct you move.");
+    }
+  }
+
+  // Send move to server  
+  sendMove(player_move);
+  
+  // Check victory conditions
+  greenVictory = checkWin(greenStatus);
+  if (greenVictory){
+    printMsg("A winner is you"); //TODO think some proper msgs
+    victoryLoop('g');
+  }
+  
+  
+  
+  // ******* Rival turn *******
+  // Check rival move, wait until it happens
+  // Update color status
+  // Check victory conditions
+  
+  
+  
+  
+  int rivalMove = getMove();
+  redStatus[rivalMove] = 1;
+  
   if (!redVictory && !greenVictory){
     updateColorStatus();
   }
+  
   
   redVictory = checkWin(redStatus);
   greenVictory = checkWin(greenStatus);
@@ -88,7 +139,7 @@ void loop() {
   } else if (greenVictory){
     victoryLoop('g');
   }
-  */
+  
 
 }
 
@@ -126,10 +177,11 @@ void initializeScreen(){
 }
 
 /**********************************************
- *   WELCOME SCREEN
+ *   SCREEN PRONTS
  **********************************************/
 
 void doWelcome(){
+  display.clear();
   display.printText("Welcome to the tictactoe game!", 6, 1, BLUE);
   delay(1000);
   display.clear();
@@ -138,26 +190,59 @@ void doWelcome(){
   while(doLoop){
     if (display.buttonsChanged()) {
       byte reply = display.getButtons();
+      // Green button
       if ((reply & 0x04) == 0x00){
           doLoop = false;
       }
     }  
   }
   display.clear();
-  display.printText("Starting new game!", 6, 1, GREEN);
-  
-  
+  display.printText("Starting new game!", 6, 1, GREEN);  
+}
+
+void msgAndWait(String msg){
+  display.clear();
+  display.printText(msg, 6, 1, RED);
+  boolean doLoop = true;
+  while(doLoop){
+    if (display.buttonsChanged()) {
+      byte reply = display.getButtons();
+      // Green button
+      if ((reply & 0x04) == 0x00){
+          doLoop = false;
+      }
+    }  
+  }
+  display.clear(); 
+}
+
+void printMsg(String msg){
+  display.clear();
+  display.printText(msg, 6, 1, BLUE);
 }
 
 /**********************************************
- *   GAME INITIALIZATION WITH THE SERVER
+ *   COMMUNICATION WITH THE GAME SERVER
  **********************************************/
 
-int findPlayer(int player_id){
+int findPlayer(){
   //TODO
-  Serial.println("finding player");
+  // rival_id = send ("REGISTER " + player_id + "\0n")
+  // returns the rival_id
   return 113;
 
+}
+
+int getMove(){
+  //TODO
+  // move = send ("GET_MOVE " + rival_id)
+  return 0;
+}
+
+void sendMove(int player_move){
+  //TODO
+  // send ("MOVE " + player_move + player_id)
+  return 0;
 }
 
 
@@ -183,6 +268,45 @@ void calibrateSensors(){
 /**********************************************
  *   GAME LOGIC
  **********************************************/
+ 
+int checkPlayerMove(){
+  int totalMoves = 0;
+  int player_move = 0;
+  char color;
+  for(int i = 0; i < 9; i++){
+    color = squares[i]->checkColor();
+    if (previousGreenStatus[i] != greenStatus[i]){
+      totalMoves++;
+      player_move = i+1;
+    }
+  }
+  
+  if (totalMoves == 0) {
+    // The player has not done any move
+    player_move = 0;
+  } else if (totalMoves > 1) {
+    // The player has moved 2 pieces, which is an illegal move
+    player_move = -1;
+  }
+  else if (color == 'r'){
+    // The player has moved his piece into an square already used by the rival
+    player_move = -2;
+  }
+  
+  return player_move;
+  
+}
+
+
+void updateBoardStatus(){
+  for (int i=0; i < 9; i++){
+    char color = squares[i]->checkColor();
+    if (color == 'g'){
+      squares[i]->setGreen();
+      greenStatus[i] = 1;
+    }
+  }
+}
 
 void updateColorStatus(){
   for (int i=0; i < 9; i++){
